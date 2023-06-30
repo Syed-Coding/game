2457d956a3882fa1e172d3f861ba65ab770184c9
Syed-Coding <syed2ktym@gmail.com>
2023-06-30 16:36:22 +0530

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Swal from "sweetalert2";
 import { GuessedList } from "./GuessedList";
 import { Modal } from "./Modal";
 import { FinalModal } from "./FinalModal";
+import { Loader } from "./Loader";
 
 function App() {
   const [originalval, setOriginalVal] = useState(
@@ -18,11 +19,13 @@ function App() {
   const [guessedValues, setGuessedValues] = useState([]);
   const [openmodal, setOpenModal] = useState(false);
   const [openfinalalert, setOpenFinalAlert] = useState(false);
+  const [loader, setLoader] = useState(true);
 
   useEffect(() => {
     setTimeout(() => {
       setOpenModal(true);
-    }, 1000);
+      setLoader(false);
+    }, 3000);
   }, []);
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -97,83 +100,87 @@ function App() {
 
   return (
     <>
+      {loader && <Loader></Loader>}
       {openmodal && <Modal setOpenModal={setOpenModal}></Modal>}
-      <div className="center">
-        <h1>GUESS THE NUMBER</h1>
-        <h2
-          style={{
-            color: "violet",
-            padding: "1px",
-            marginBottom: "35px",
-            textTransform: "uppercase",
-          }}
-        >
-          Guess a number from 0-100
-        </h2>
-        <h4 style={{ padding: "1px", marginBottom: "35px" }}>
-          GUESSES LEFT : <span style={{ color: "red" }}>{`${retries}`}</span>
-        </h4>
 
-        {hidelevelbtns && (
-          <Level
-            setretries={setretries}
-            setHideLevelBtns={setHideLevelBtns}
-            setUserDifficulty={setUserDifficulty}
-          ></Level>
-        )}
-        {retries !== 0 && !hidelevelbtns && (
-          <form onSubmit={handleSubmit}>
-            <input
-              className="number-input"
-              type="number"
-              placeholder="Guess The Number"
-              value={userval}
-              onChange={(e) => {
-                setUserVal(e.target.value > 100 ? 0 : e.target.value);
-              }}
-              autoFocus
-            />
-            <br></br>
+      {!loader && (
+        <div className="center">
+          <h1>GUESS THE NUMBER</h1>
+          <h2
+            style={{
+              color: "violet",
+              padding: "1px",
+              marginBottom: "35px",
+              textTransform: "uppercase",
+            }}
+          >
+            Guess a number from 0-100
+          </h2>
+          <h4 style={{ padding: "1px", marginBottom: "35px" }}>
+            GUESSES LEFT : <span style={{ color: "red" }}>{`${retries}`}</span>
+          </h4>
 
-            <button
-              className={
-                retries <= 2
-                  ? "btn btn-lg btn-danger"
-                  : "btn btn-lg btn-primary"
-              }
-              type="submit"
-            >
-              {retries <= 2 ? "OH !!! No 😱" : "GUESS ME 😊"}
-            </button>
-          </form>
-        )}
+          {hidelevelbtns && (
+            <Level
+              setretries={setretries}
+              setHideLevelBtns={setHideLevelBtns}
+              setUserDifficulty={setUserDifficulty}
+            ></Level>
+          )}
+          {retries !== 0 && !hidelevelbtns && (
+            <form onSubmit={handleSubmit}>
+              <input
+                className="number-input"
+                type="number"
+                placeholder="Guess The Number"
+                value={userval}
+                onChange={(e) => {
+                  setUserVal(e.target.value > 100 ? 0 : e.target.value);
+                }}
+                autoFocus
+              />
+              <br></br>
 
-        {openfinalalert && (
-          <FinalModal
-            setOpenFinalAlert={setOpenFinalAlert}
-            originalval={originalval}
-          ></FinalModal>
-        )}
-        {retries === 0 &&
-          (setOriginalVal(Math.floor(Math.random() * 101)),
-          setretries("PLAY AGAIN ? SELECT ONE MODE TO PLAY THE GAME"),
-          setHideLevelBtns(true),
-          setOpenFinalAlert(true),
-          setGuessedValues([]),
-          setUserVal(""))}
-        {userdiificult === "medium" && (
-          <GuessedList guessedValues={guessedValues}></GuessedList>
-        )}
-        {userdiificult === "easy" && (
-          <GuessedList guessedValues={guessedValues}></GuessedList>
-        )}
-        {retries <= 5 && userdiificult === "easy" && (
-          <Hint originalval={originalval}></Hint>
-        )}
-        {retries <= 3 && userdiificult === "medium" && (
-          <Hint originalval={originalval}></Hint>
-        )}
-      </div>
+              <button
+                className={
+                  retries <= 2
+                    ? "btn btn-lg btn-danger"
+                    : "btn btn-lg btn-primary"
+                }
+                type="submit"
+              >
+                {retries <= 2 ? "OH !!! No 😱" : "GUESS ME 😊"}
+              </button>
+            </form>
+          )}
+
+          {openfinalalert && (
+            <FinalModal
+              setOpenFinalAlert={setOpenFinalAlert}
+              originalval={originalval}
+            ></FinalModal>
+          )}
+          {retries === 0 &&
+            (setOriginalVal(Math.floor(Math.random() * 101)),
+            setretries("PLAY AGAIN ? SELECT ONE MODE TO PLAY THE GAME"),
+            setHideLevelBtns(true),
+            setOpenFinalAlert(true),
+            setGuessedValues([]),
+            setUserVal(""))}
+          {userdiificult === "medium" && (
+            <GuessedList guessedValues={guessedValues}></GuessedList>
+          )}
+          {userdiificult === "easy" && (
+            <GuessedList guessedValues={guessedValues}></GuessedList>
+          )}
+          {retries <= 5 && userdiificult === "easy" && (
+            <Hint originalval={originalval}></Hint>
+          )}
+          {retries <= 3 && userdiificult === "medium" && (
+            <Hint originalval={originalval}></Hint>
+          )}
+        </div>
+      )}
     </>
   );
 }
